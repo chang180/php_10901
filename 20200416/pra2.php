b0580c5f7e8f182b4213c2aff0cbc58d6bd2ec22
chang180 <chang180@gmail.com>
2020-04-16 09:45:33 +0800

    字串處理教學完成

---
+++ 20200416/pra2.php
@@ -0,0 +1,15 @@
+<h4>字串"好的，該死的新冠肺炎"，請將"新冠肺炎"四字放大並變色</h4>
+<?php
+$str="好的，該死的新冠肺炎";
+// $arr=explode('',$str);
+// print_r($arr);
+echo str_replace("新冠肺炎","<span style='color:red;font-size:24px'>武漢肺炎</span>",$str),"<hr>";
+
+$search="該死的";
+$target="<span style='color:darkseablue;font-size:24px;font-family:\"微軟正黑體\"'>可怕的</span>";
+
+echo str_replace($search,$target,$str),"<hr>";
+
+?>
+<hr>
+好的，該死的<span style="color:red;font-size:24px">新冠肺炎</span>
