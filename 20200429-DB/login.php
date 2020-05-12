6a1dcab42982d88c11134d25157f0be172edbf34
張 建文 <chang180@gmail.com>
2020-05-12 21:09:53 +0800

    dice.html finished

--- 20200429-DB/login.php
+++ 20200429-DB/login.php
@@ -9,55 +9,58 @@
 </head>
 <style>
     body {
+        margin:0;
+        padding:0;
         font-family: "微軟正黑體", sans-serif;
-        text-align: center;
-        background: url(bg.jpg);
+        /* background: url(bg.jpg);
         background-repeat: no-repeat;
-        background-size: cover;
+        background-size: cover; */
         /* background-blend-mode: screen; */
 
     }
 </style>
 
 <body>
-    <table>
-        <tr>
-            <td colspan="2">
-                <?php
-                if (isset($_GET['status'])) {
-                    // if($_GET['status']=='false'){
-                    //     echo "登入失敗";
-                    // }
-                    switch ($_GET['status']) {
-                        case 'false':
-                            echo "登入失敗";
-                            break;
-                        case 'true':
-                            header("location:listUser.php?acc=" . $_GET['acc']);
-                            break;
+    <div class="container w-25 mt-5 border shadow rounded-lg">
+        <table>
+            <tr>
+                <td colspan="2">
+                    <?php
+                    if (isset($_GET['status'])) {
+                        // if($_GET['status']=='false'){
+                        //     echo "登入失敗";
+                        // }
+                        switch ($_GET['status']) {
+                            case 'false':
+                                echo "登入失敗";
+                                break;
+                            case 'true':
+                                header("location:listUser.php?acc=" . $_GET['acc']);
+                                break;
+                        }
                     }
-                }
-                ?>
-            </td>
-        </tr>
-    </table>
-    <!-- 稍微加點bootstrap進去，先讓表單別太難看就好 -->
-    <div class="container-fluid w-25 p-2 bg-info text-light border shadow rounded">
-        <h2>使用者登入</h2>
-        <form class="form" action="checkLogin.php" method="post">
-            <div class="form-group">
-                <label class="form" for="username">帳號：</label>
-                <input class="form" type="text" name="acc" id="acc">
-            </div>
-            <div class="form-group">
-                <label class="form" for="password">密碼：</label>
-                <input class="form" type="password" name="pw" id="pw">
-            </div>
-            <div class="btn-group">
-                <input class="btn btn-primary rounded-circle" type="submit" name="submit" value="登入">
-                <input class="btn btn-secondary rounded-circle" type="reset" value="重置">
-            </div>
-        </form>
+                    ?>
+                </td>
+            </tr>
+        </table>
+        <!-- 稍微加點bootstrap進去，先讓表單別太難看就好 -->
+        <div class="container-fluid bg-info text-light">
+            <h2>使用者登入</h2>
+            <form class="form" action="checkLogin.php" method="post">
+                <div class="form-group">
+                    <label class="form" for="username">帳號：</label>
+                    <input class="form" type="text" name="acc" id="acc">
+                </div>
+                <div class="form-group">
+                    <label class="form" for="password">密碼：</label>
+                    <input class="form" type="password" name="pw" id="pw">
+                </div>
+                <div class="btn-group">
+                    <input class="btn btn-primary rounded-circle" type="submit" name="submit" value="登入">
+                    <input class="btn btn-secondary rounded-circle" type="reset" value="重置">
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 
