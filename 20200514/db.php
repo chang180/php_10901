44d92edf7dcdc52fbd74fba0ee2bdf810abe456c
chang180 <chang180@gmail.com>
2020-05-14 14:40:21 +0800

    自訂函式的應用

---
+++ 20200514/db.php
@@ -0,0 +1,46 @@
+<?php
+
+$dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+$pdo=new PDO($dsn,'root','');
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+// print_r(all('invoice'));
+$rows=all('invoice');
+// print_r($rows);
+
+foreach($rows as $row){
+    echo $row['number'],'-',$row['expense'],'<br>';
+}
+echo "<hr>";
+
+$row=find('invoice',1);
+print_r(find('invoice','1')); 
+if(is_array($row)){
+echo "<br>",$row['number'],"<br>";  
+echo $row['expense'],"<br>";
+}else{
+    echo $row;
+}
+
+
+function all($table){
+    global $pdo;
+    $sql="SELECT * FROM $table";
+    $rows=$pdo->query($sql)->fetchAll();
+    return $rows;
+}
+
+function find($table,$id){
+    global $pdo;
+    $sql="SELECT * FROM $table WHERE `id`='$id'";
+    $row=$pdo->query($sql)->fetch();
+    if(empty($row)){
+        return "無符合資料的內容<br>";
+    }
+    return $row;
+}
+
+
+
+?>
