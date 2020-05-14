cff04d96fd94da9fe3289f6190ebc7ee22db18d1
chang180 <chang180@gmail.com>
2020-05-14 16:26:00 +0800

    all方法中加入第3個字串變數

--- 20200514/all.php
+++ 20200514/all.php
@@ -27,6 +27,7 @@ function all($table,...$arg){
     
     $sql="SELECT * FROM $table ";
 
+    // 若第二個之後的參數存在
     if(isset($arg[0]) && is_array($arg[0])){
         $tmp=[];
         foreach($arg[0] as $key => $value){
@@ -37,6 +38,11 @@ function all($table,...$arg){
 // print_r($tmp);
 
         $sql=$sql." WHERE " . implode(" && ",$tmp);
+    }
+
+    // 第三個參數存在時，將條件加入查詢語法，不判斷資料型態，使用時自己注意
+    if(isset($arg[1])){
+        $sql=$sql.$arg[1];
     }
     
     // echo $sql;
@@ -53,21 +59,42 @@ foreach($rows as $row){
     echo $row['code'].'-';
     echo $row['number'].'-';
     echo $row['period'].'-';
-    echo $row['expense'].'-';
+    echo $row['expense'];
     echo '<hr>';
 }
 
-echo "有帶參數<br>";
+echo "1.有帶參數<br>";
 $rows=all('invoice',["year"=>"2020","period"=>"1"]);
 foreach($rows as $row){
     echo $row['id'].'-';
     echo $row['code'].'-';
     echo $row['number'].'-';
     echo $row['period'].'-';
-    echo $row['expense'].'-';
+    echo $row['expense'];
     echo '<hr>';
 }
 
+echo "2.有帶三個參數<br>";
+$rows=all('invoice',["year"=>"2020","period"=>"1"]," ORDER BY id DESC");
+foreach($rows as $row){
+    echo $row['id'].'-';
+    echo $row['code'].'-';
+    echo $row['number'].'-';
+    echo $row['period'].'-';
+    echo $row['expense'];
+    echo '<hr>';
+}
+
+echo "3.不設定第2個參數，會使用第3個參數條件全部列出<br>";
+$rows=all('invoice',""," ORDER BY id DESC");
+foreach($rows as $row){
+    echo $row['id'].'-';
+    echo $row['code'].'-';
+    echo $row['number'].'-';
+    echo $row['period'].'-';
+    echo $row['expense'];
+    echo '<hr>';
+}
 
 // 不定參數函式，即使沒有下第2個以後的參數，也不會有錯誤
 echo "<hr><pre>";
