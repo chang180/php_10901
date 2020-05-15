4744dbfd91c050527999ed8394c880c351d2656d
chang180 <chang180@gmail.com>
2020-05-15 10:53:34 +0800

    示範自訂函式

---
+++ 20200514/array_merge.php
@@ -0,0 +1,49 @@
+<?php
+
+$a=["AA","BB"];
+$b=["CC","DD"];
+$d=["XX","YY","ZZ"];
+
+
+$c=array_merge($a,$b,$d);
+
+echo "<pre>";
+print_r($c);
+echo "</pre><hr>";
+
+echo "自己寫的array_merge():<br>";
+echo "<pre>";
+print_r($c);
+echo "</pre><br>";
+
+$test=["張","建","文"];
+$my=am($d,$b,$a,$test);
+echo "<pre>";
+print_r($my);
+echo "</pre>";
+
+// function am(...$aa){
+//     $arr=[];
+// if (is_array($aa)){
+//     foreach($aa as $val){
+        
+//         $arr[]=$val;
+//     }
+// }
+// return $arr;
+// }
+
+function am($a,...$b){
+    $arr=[];
+    if(is_array($a)){
+        $arr=$a;
+    }else{
+        echo "not an array";
+    }
+    foreach($b as $val){
+        foreach($val as $item){
+            $arr[]=$item;
+        }
+    }
+    return $arr;
+}
